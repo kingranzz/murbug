ab79820b2f3afd12facec5ebc97f7a603f28ea6b
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-02-08 19:29:14 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  BOT_TOKEN: "7549474841:AAG78UjaCRBwE63TNlNst_ZEc-hjJcwREjU",
+  BOT_TOKEN: "7350299617:AAErmJwSbIxzTqPgP8L4URBX1O6BVqpTa1Y",
     allowedDevelopers: ['5965707646'], // ID
 };
